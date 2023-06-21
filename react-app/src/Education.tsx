2e9df478d8f73a895186666174214e055464ea70
YingLiuM <130420630+YingLiuM@users.noreply.github.com>
2023-06-21 14:03:06 -0400

    create components

---
+++ react-app/src/Education.tsx
@@ -0,0 +1,8 @@
+function Education(){
+    var university = "York University";
+    var year = 2022;
+    var major = "Information Technology"
+    return <h2>I gruaduate form {university} in {year}, and I major in {major}</h2>
+}
+
+export default Education
